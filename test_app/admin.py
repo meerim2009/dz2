0dde196d56620d753296b5d60a3c1806c3b91672
Meerim <meerim@Meerims-MacBook-Air.local>
2021-05-05 19:00:31 +0600

    asasas

---
+++ test_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from test_app.models import Category, Product, Review
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Review)
---
+++ test_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from test_app.models import Category, Product, Review
+
+
